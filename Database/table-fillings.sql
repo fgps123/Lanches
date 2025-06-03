5586a92a475c3c98db5439c14dc31c9d1553b41c
fgps123 <guilhermesilva2022.gs@gmail.com>
2025-06-03 10:01:45 -0300

    Update table-fillings.sql

--- Database/table-fillings.sql
+++ Database/table-fillings.sql
@@ -8,6 +8,10 @@ create table fillings (
 )
 
 insert into fillings values (1, 2, 'Queijo', 1.50),
-							 (3, 2, 'Nutella', 5.50)
+	                    (2, 2, 'Presunto', 1.20),
+			    (3, 2, 'Nutella', 5.50),
+	                    (1, 2, 'Queijo', 1.50),
+	                    (2, 2, 'Presunto', 1.50)
+			    (3, 2, 'Nutella', 5.50)
 
 select name, price from fillings where id_foods = 1
